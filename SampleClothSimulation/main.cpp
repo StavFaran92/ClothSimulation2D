3ef3a079adecc19fa1d72816aa2d6c40f930683e
stav faran <stav12212@gmail.com>
2022-11-30 19:48:01 +0200

    cloth is working

--- SampleClothSimulation/main.cpp
+++ SampleClothSimulation/main.cpp
@@ -19,28 +19,154 @@ using vec3 = glm::vec3;
 #define logWarning(msg) std::cout << msg << std::endl;
 #define logError(msg) std::cout << msg << std::endl;
 
-struct Particle
+class ParticleSystem;
+
+class Particle
 {
+public:
+	Particle();
+	Particle(const vec3& pos);
+	vec3 getCurrentPos() const;
+	vec3 getPreviousPos() const;
+	void setPos(const vec3& pos);
+	void move(const vec3& pos);
+	void addforce(const vec3& force);
+	void resetForces();
+	void update(float dt);
+	void pin();
+
+private:
 	vec3 m_currentPos;
 	vec3 m_previousPos;
 	vec3 m_forceAccumulations;
+	bool m_isPinned = false;
 };
 
-struct Constraint
+Particle::Particle()
+	: m_currentPos({}), m_previousPos(m_currentPos), m_forceAccumulations()
 {
-	int particleA;
-	int particleB;
-	float restLength;
+}
+
+Particle::Particle(const vec3& pos)
+	: m_currentPos(pos), m_previousPos(m_currentPos), m_forceAccumulations()
+{
+}
+
+vec3 Particle::getCurrentPos() const
+{
+	return m_currentPos;
+}
+
+vec3 Particle::getPreviousPos() const
+{
+	return m_previousPos;
+}
+
+void Particle::setPos(const vec3& pos)
+{
+	m_currentPos = pos;
+}
+
+void Particle::move(const vec3& pos)
+{
+	if (m_isPinned)
+		return;
+
+	m_currentPos += pos;
+}
+
+void Particle::addforce(const vec3& force)
+{
+	m_forceAccumulations += force;
+}
+
+void Particle::resetForces()
+{
+	m_forceAccumulations = {};
+}
+
+void Particle::update(float dt)
+{
+	if (m_isPinned)
+		return;
+
+	vec3& x = m_currentPos;
+	vec3 temp_x = x;
+	vec3& old_x = m_previousPos;
+	vec3& a = m_forceAccumulations;
+	x += (x - old_x) + a * (dt * dt);
+	old_x = temp_x;
+}
+
+void Particle::pin()
+{
+	m_isPinned = true;
+}
+
+class Constraint
+{
+public:
+	Constraint(Particle* particleA, Particle* particleB, float restLength);
+	Particle* getParticleA() const;
+	Particle* getParticleB() const;
+	float getRestLength() const;
+private:
+	Particle* m_particleA = nullptr;
+	Particle* m_particleB = nullptr;
+	float m_restLength;
+};
+
+Constraint::Constraint(Particle* particleA, Particle* particleB, float restLength)
+	: m_particleA(particleA), m_particleB(particleB), m_restLength(restLength)
+{}
+
+Particle* Constraint::getParticleA() const
+{
+	return m_particleA;
+}
+
+Particle* Constraint::getParticleB() const
+{
+	return m_particleB;
+}
+
+float Constraint::getRestLength() const
+{
+	return m_restLength;
+}
+
+class Stick
+{
+public:
+	void draw() const;
+private:
+	Constraint m_constraint;
+
+};
+
+void Stick::draw() const
+{
+}
+
+
+class Cloth
+{
+public:
+	Cloth(ParticleSystem& ps, const vec3& pos, int sizeH, int sizeV, int gridSize);
+private:
+
 };
 
 class ParticleSystem
 {
 public:
 	ParticleSystem();
+	~ParticleSystem();
 	void step(float dt);
 	void addConstraint(const Constraint& c);
-	void addParticle(const Particle& p);
-	void draw(std::function<void(const Particle&)> cb) const;
+	//void addParticle(const Particle& p);
+	void addParticle(Particle* p);
+	void draw(std::function<void(const Particle* particle)> cb) const;
 private:
 	void verlet(float dt);
 	void satisfyContraints();
@@ -51,13 +177,21 @@ private:
 	vec3 m_gravity = {0, 9.81f, 0};
 	float m_timestamp = TIMESTAMP;
 
-	std::vector<Particle> m_particles;
+	std::vector<Particle*> m_particles;
 	std::vector<Constraint> m_constraints;
 };
 
 ParticleSystem::ParticleSystem()
 {
 
+}
+
+ParticleSystem::~ParticleSystem()
+{
+	for (int i = m_particles.size() - 1; i >= 0; i--)
+	{
+		delete m_particles[i];
+	}
 }
 
 void ParticleSystem::step(float dt)
@@ -71,12 +205,7 @@ void ParticleSystem::verlet(float dt)
 {
 	for (auto& p : m_particles)
 	{
-		vec3& x = p.m_currentPos;
-		vec3 temp_x = x;
-		vec3& old_x = p.m_previousPos;
-		vec3& a = p.m_forceAccumulations;
-		x += (x - old_x) + a * (dt * dt);
-		old_x = temp_x;
+		p->update(dt);
 	}
 }
 
@@ -87,25 +216,21 @@ void ParticleSystem::satisfyContraints()
 		//satisfy bounds
 		for (auto& p : m_particles)
 		{
-			vec3& v = p.m_currentPos;
-			v = glm::min(glm::max(v, { 0,0,0 }), { WIDTH,HEIGHT,HEIGHT });
+			vec3 v = p->getCurrentPos();
+			p->setPos(glm::min(glm::max(v, { 0,0,0 }), { WIDTH,HEIGHT,HEIGHT }));
 		}
 
 		// satisfy contsraints
 		for (const auto& c : m_constraints)
 		{
-			vec3& v1 = m_particles[c.particleA].m_currentPos;
-			vec3& v2 = m_particles[c.particleB].m_currentPos;
+			vec3 v1 = c.getParticleA()->getCurrentPos();
+			vec3 v2 = c.getParticleB()->getCurrentPos();
 			vec3 delta = v2 - v1;
 			float deltaLength = glm::length(delta);
-			float diff = (deltaLength - c.restLength) / deltaLength;
-			v1 += delta * .5f * diff;
-			v2 -= delta * .5f * diff;
+			float diff = (deltaLength - c.getRestLength()) / deltaLength;
+			c.getParticleA()->move(delta * .5f * diff);
+			c.getParticleB()->move(-(delta * .5f * diff));
 		}
-
-		// Constrain one particle of the cloth to orig
-		m_particles[0].m_currentPos = { WIDTH / 2 , HEIGHT / 2, 0 };
-
 	}
 }
 
@@ -113,7 +238,8 @@ void ParticleSystem::accumulateForces()
 {
 	for (auto& p : m_particles)
 	{
-		p.m_forceAccumulations = m_gravity;
+		p->resetForces();
+		p->addforce(m_gravity);
 	}
 }
 
@@ -122,12 +248,12 @@ void ParticleSystem::addConstraint(const Constraint& c)
 	m_constraints.push_back(c);
 }
 
-void ParticleSystem::addParticle(const Particle& p)
+void ParticleSystem::addParticle(Particle* p)
 {
 	m_particles.push_back(p);
 }
 
-void ParticleSystem::draw(std::function<void(const Particle& particle)> cb) const
+void ParticleSystem::draw(std::function<void(const Particle* particle)> cb) const
 {
 	for (const auto& p : m_particles)
 	{
@@ -136,16 +262,46 @@ void ParticleSystem::draw(std::function<void(const Particle& particle)> cb) cons
 	
 }
 
+Cloth::Cloth(ParticleSystem& ps, const vec3& pos, int sizeH, int sizeV, int gridSize)
+{
+	const vec3& origin = pos;
+
+	std::vector<Particle*> particles;
+	particles.reserve(sizeH * sizeV);
+
+	for (int i = 0; i < sizeV; i++)
+	{
+		for (int j = 0; j < sizeH; j++)
+		{
+			Particle* p = new Particle(origin + vec3{ j * gridSize, i * gridSize, 0 });
+
+			if (i != 0)
+			{
+				Constraint c(p, particles.at((i - 1) * sizeV + j), gridSize);
+				ps.addConstraint(c);
+			}
+
+			if (j != 0)
+			{
+				Constraint c(p, particles.back(), gridSize);
+				ps.addConstraint(c);
+			}
+
+			particles.push_back(p);
+			ps.addParticle(p);
+		}
+	}
+
+	// Constrain one particle of the cloth to orig
+	particles.at(0)->pin();
+	particles.at(sizeH-1)->pin();
+}
+
 int main()
 {
 	ParticleSystem ps;
 
-	vec3 origin = { WIDTH / 2, HEIGHT / 2, 0 };
-
-	ps.addParticle({ origin, origin, vec3{ 0, 0, 0 } });
-	ps.addParticle({ origin + vec3{100,0,0}, origin + vec3{ 100,0,0 }, vec3{ 0, 0, 0 } });
-
-	ps.addConstraint({ 0, 1, 100 });
+	Cloth cloth(ps, vec3(100, 100, 0), 10, 10, 10);
 	
 	sf::RenderWindow window(sf::VideoMode({ WIDTH, HEIGHT }), "Cloth simulation", sf::Style::Close);
 
@@ -166,21 +322,13 @@ int main()
 			// Escape key: exit
 			if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape))
 				window.close();
-
-			// Escape key: exit
-			if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::A))
-				ps.step(1.f);
 		}
 
 		ps.step(.1f);
 
-		//sf::Time time = clock.restart();
-		//float dt = (float)time.asMilliseconds() / 1000;
-		//ps.step(dt);
-
-		ps.draw([&](const Particle& particle) {
+		ps.draw([&](const Particle* particle) {
 			sf::Vertex point[1];
-			point[0].position = sf::Vector2f(particle.m_currentPos.x, particle.m_currentPos.y);
+			point[0].position = sf::Vector2f(particle->getCurrentPos().x, particle->getCurrentPos().y);
 			point[0].color = sf::Color::Red;
 			window.draw(point, 1, sf::Points);
 		});
@@ -189,3 +337,4 @@ int main()
 		window.display();
 	}
 }
+
